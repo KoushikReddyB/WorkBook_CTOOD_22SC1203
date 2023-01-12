84fcca11dd1f66870cdc8aef789b7470bd9b1cd9
KoushikReddyB <2200090018@kluniversity.in>
2023-01-12 10:59:23 +0530

    week 02 update

--- Week 02 (pg 32 to pg 46)/Post-Lab Task (pg38 to pg 39)/01_Armstrong number.java
+++ Week 02 (pg 32 to pg 46)/Post-Lab Task (pg38 to pg 39)/01_Armstrong number.java
@@ -1,1 +1,35 @@
-/* 1. Write a class Armstrong with 2 static methods is Armstrong() to check whether a given number is Armstrong or not and display Armstrong) to displays all Armstrong numbers up ton". Acces them using main () method from the same class. */
+/* 1. Write a class Armstrong with 2 static methods is Armstrong() to check whether a given number is Armstrong or not and display Armstrong() to displays all Armstrong numbers up to n". Acces them using main () method from the same class. */
+
+package week02;
+import java.util.*;
+public class Armstrong {
+    public static boolean isArmstrong(int n) {
+        int rem,temp=n,sum=0;
+        while(n>0) {
+            rem=n%10;
+            sum=sum+(rem*rem*rem);
+            n=n/10;
+        }
+        if(temp==sum) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    public static void displayArmstrong(int n) {
+        for(int i=1;i<=n;i++){
+        	boolean val=Armstrong.isArmstrong(i);
+            if(val==true) {
+                System.out.println(i);
+            }
+        }
+	}
+    public static void main(String[] args){
+        Scanner in=new Scanner(System.in);
+        System.out.print("Enter number > ");
+        int n=in.nextInt();
+        in.close();
+        Armstrong.displayArmstrong(n);
+    }
+}
