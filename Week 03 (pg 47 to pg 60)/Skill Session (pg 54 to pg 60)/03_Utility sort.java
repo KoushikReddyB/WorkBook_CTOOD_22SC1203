570bb67101fd527a2f2f9e3d55e0cce2f7eab78f
KoushikReddyB <2200090018@kluniversity.in>
2023-01-24 15:21:30 +0530

    week 03 update

--- Week 03 (pg 47 to pg 60)/Skill Session (pg 54 to pg 60)/03_Utility sort.java
+++ Week 03 (pg 47 to pg 60)/Skill Session (pg 54 to pg 60)/03_Utility sort.java
@@ -1,3 +1,61 @@
 /* 3. Enhance a Utility class from session no 7 by adding three more static methods. which overload existing Sort () which accepts an array of double to sort them, findSmallest () which accepts an array of double to find smallest value, findLargest () which accepts an array of double to find largest value Access overloading methods from main () of Demo class of different package. (Take input through command line.) */
 
-/* NEED TO BE UPDATED... */
+/* Utility Class */
+package week03;
+public class Utility {
+	public static void sort(double a[])	{
+		int i,j;
+		double t;
+		for(i=1;i<a.length;i++) {
+			for(j=i;j<a.length-1;j++) {
+				if(a[j]>a[j+1]) {
+					t=a[j];
+					a[j]=a[j+1];
+					a[j+1]=t;
+				}
+			}
+		}
+		for(i=0;i<a.length;i++)
+			System.out.println(a[i]);
+	}
+	public static double findSmallest(double a[]) {
+		double min=a[0];
+		for(int i=0;i<a.length;i++) {
+			if(a[i]<min) {
+				min=a[i];
+			}
+		}
+		return min;
+	}
+	public static double findLargest(double a[]) {
+		double max=a[0];
+		for(int i=0;i<a.length;i++) {
+			if(a[i]>max) {
+				max=a[i];
+			}
+		}
+		return max;
+	}
+}
+
+/* Demo Class */
+package week02;
+import java.util.*;
+public class Demo {
+
+	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);
+		int n=in.nextInt();
+		double da[]=new double[n];
+		int i=0;
+		for(String s:args){
+			da[i]=Double.parseDouble(s);
+			i++;
+		}
+		in.close();
+		Utility.sort(da);
+		double min=Utility.findSmallest(da),max=Utility.findLargest(da);
+		System.out.println("Max: "+max+"\nMin :"+min);
+	}
+
+}
--- Week 03 (pg 47 to pg 60)/Skill Session (pg 54 to pg 60)/06_Palindrome Numbers.java
+++ Week 03 (pg 47 to pg 60)/Skill Session (pg 54 to pg 60)/06_Palindrome Numbers.java
@@ -1,3 +1,39 @@
 /* 6. Enhance the class Palindrome of Problem W02-P04 with another static method isPalindrome() to check whether an each element array is palindrome or not. If all the elements in the array are palindrome numbers then return true otherwise, return false. Access them using main () from the same class. (Take hard coded value for the variable n) */
 
-/* NEED TO BE UPDATED - ISSUE WITH THE EXECUTION OF CODE... */
+package week02;
+import java.util.*;
+public class Palindrome {
+	public static int findReverse(int n) {
+		int rev=0,reminder;
+		while(n!=0) {
+			reminder=n%10;
+			rev=(rev*10)+reminder;
+			n=n/10;
+		}
+		return rev;
+	}
+	public static boolean isPalindrome(int n) {
+		int temp=n;
+		if(temp==Palindrome.findReverse(n)) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	public static void displayAllPalindromeNOs(int n) {
+        for(int i=1;i<=n;i++){
+        	boolean val=Palindrome.isPalindrome(i);
+            if(val==true) {
+                System.out.println(i);
+            }
+        }
+	}
+	public static void main(String[] args) {
+		Scanner in=new Scanner(System.in);
+		System.out.print("Enter a number > ");
+		int n=in.nextInt();
+		in.close();
+		Palindrome.displayAllPalindromeNOs(n);	
+	}
+}
